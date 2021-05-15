82db5467c8099baf943cafc036d769c7dc42ad0d
Sayduganov <mrruslan733@gmail.com>
2021-05-16 00:01:31 +0500

    Сайдуганов сделал изменения

--- src/main/java/create/CreatePDF.java
+++ src/main/java/create/CreatePDF.java
@@ -53,7 +53,7 @@ public class CreatePDF {
 			e.printStackTrace();
 		}
 		
-		String string_pdf = "Добрый день замечательные группы ПИ второго курса кафедры АСУ УГАТУ! Тестовое приложения для создания PDF файла.";
+		String string_pdf = "Добрый день замечательные группы ПИ второго курса Факультета ИРТ кафедры АСУ УГАТУ ! Тестовое приложения для создания PDF файла.";
 		Paragraph paragraph = new Paragraph();
 	    paragraph.add(new Paragraph(string_pdf, new Font(times,14)));
 	    
